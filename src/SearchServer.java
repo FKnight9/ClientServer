826a635bdd3f351bb275be1691adaff922339834
Faris Ibrahim <faristheknight9@gmail.com>
2018-04-24 20:47:41 -0400

    Connection has been established between client and server files on different computers

--- src/SearchServer.java
+++ src/SearchServer.java
@@ -1,10 +1,14 @@
 //program for server application
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.rmi.*;
 import java.rmi.registry.*;
 public class SearchServer
 {
-    public static void main(String args[])
+    public static void main(String args[]) throws UnknownHostException
     {
+    	String hostname = InetAddress.getLocalHost().getHostAddress();
+    	System.out.println("this host IP is " + hostname);
         try
         {
             // Create an object of the interface
@@ -13,12 +17,12 @@ public class SearchServer
  
             // rmiregistry within the server JVM with
             // port number 1900
-            LocateRegistry.createRegistry(1900);
+            LocateRegistry.createRegistry(5002);
  
             // Binds the remote object by the name
             // geeksforgeeks
-            Naming.rebind("rmi://localhost:1900"+
-                          "/geeksforgeeks",obj);
+            Naming.rebind("rmi://155.246.140.51:5002"+
+                          "/CPE545",obj);
         }
         catch(Exception ae)
         {
