8f44179342ec43f85a1c0df935303734223a7c31
Faris Ibrahim <faristheknight9@gmail.com>
2018-04-25 12:54:19 -0400

    Made list of student and created Student class to handle

--- src/SearchQuery.java
+++ src/SearchQuery.java
@@ -1,6 +1,8 @@
 // Java program to implement the Search interface
 import java.rmi.*;
 import java.rmi.server.*;
+import java.util.ArrayList;
+import java.util.List;
 public class SearchQuery extends UnicastRemoteObject
                          implements Search
 {
@@ -9,6 +11,23 @@ public class SearchQuery extends UnicastRemoteObject
     SearchQuery() throws RemoteException
     {
         super();
+        System.out.println("Reached here");
+        listStudents();
+    }
+    
+    private String[] Students = new String[]{"Rahul","Arora","Xinqi","Bao","Thomas","Burrell","Siyu","Chen","Athina","Cordero","Priyanka","Dassani","Omkar","Desai","Yifan","Ding","Thomas","Falsone","Yubao","Geng","Manideep","Gillela","Aakanksha","Gokhe","Ao","Hu","Faris","Ibrahim","Bryan","Jimenez-Rojas","Xin","Li","Ziyue","Liu","Gary","Marshuetz","Divakar","Mer","Madjid","Mousavi","Shivam Patel","Svetlin","Pavlov","Shreyansh","Sharma","Carlos","Tejada","Juan","Vega","Oscar","Victorero","Yicong","Wang","Xuetong","Wei","Yongqi","Yan"};
+    
+    //List of Students
+    private List<Student> listStudents(){
+    	List<Student> list = new ArrayList<Student>();
+    	
+    	for(int i = 1; i < Students.length; i = i+2) {
+    		Student student = new Student();
+        	student.setFirstName(Students[i-1]);
+        	student.setLastName(Students[i]);
+        	list.add(student);
+    	}
+    	return list;
     }
  
     // Implementation of the query interface
---
+++ src/Student.java
@@ -0,0 +1,18 @@
+
+public class Student {
+	private String firstName, lastName;
+	//Getters
+	public String getFirstName() {
+		return firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	//Setters
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+}
