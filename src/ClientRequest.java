77a94a0f4bc09bdc6c86d11bed22f8a8b57e0284
athinacordero <acordero@stevens.edu>
2018-04-25 18:11:13 -0400

    Client changes (not much) Wednesday class

--- src/ClientRequest.java
+++ src/ClientRequest.java
@@ -1,19 +1,21 @@
 //program for client application
 import java.rmi.*;
+import java.util.Scanner;
 public class ClientRequest
 {
     public static void main(String args[])
     {
-        String answer,value="Reflection in Java"; // value = userinput
-        // 
+        String answer,value; // value = userinput
+        Scanner sc = new Scanner(System.in);
+        value = sc.next();
         try
         {
             // lookup method to find reference of remote object
             Search access =
-                (Search)Naming.lookup("rmi://155.246.140.51:5002"+"/CPE545");
+                (Search)Naming.lookup("rmi://155.246.140.51:5002"+"/CPE545"); //Change based on Faris
             answer = access.query(value);
-            System.out.println("Article on " + value +
-                            " " + answer+" at GeeksforGeeks");
+            System.out.println("Students name(s) " + value +
+                            " " + answer);
         }
         catch(Exception ae)
         {
