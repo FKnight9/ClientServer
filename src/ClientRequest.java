ae81aa0e917fd1f3eb4b5f179ad436536e596d11
athinacordero <acordero@stevens.edu>
2018-04-24 20:47:07 -0400

    adding client code

--- src/ClientRequest.java
+++ src/ClientRequest.java
@@ -4,13 +4,13 @@ public class ClientRequest
 {
     public static void main(String args[])
     {
-        String answer,value="Reflection in Java";
+        String answer,value="Reflection in Java"; // value = userinput
+        // 
         try
         {
             // lookup method to find reference of remote object
             Search access =
-                (Search)Naming.lookup("rmi://localhost:1900"+
-                                      "/geeksforgeeks");
+                (Search)Naming.lookup("rmi://155.246.140.51:5002"+"/CPE545");
             answer = access.query(value);
             System.out.println("Article on " + value +
                             " " + answer+" at GeeksforGeeks");
